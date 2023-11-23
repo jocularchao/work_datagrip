31dfb6d0d065d450c5ce3418e238f4fdd747ddee
jocularchao <jocularchao@gmail.com>
2023-11-23 18:21:25 +0800

    删除数据

---
+++ study_mysql/02mysql basis/1.6DML-03删除数据.sql
@@ -0,0 +1,14 @@
+-- 删除数据
+
+-- delete
+delete from test where true;
+
+insert into test (create_time)
+values ('2000-10-12 12:00:00');
+
+-- truncate
+truncate test;
+
+insert into test (create_time)
+values ('2000-10-12 12:00:00');
+
