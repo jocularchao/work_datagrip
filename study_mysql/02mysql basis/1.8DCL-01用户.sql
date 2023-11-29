c8c3a477076f8a770f4cfaca6fe28e1ff061259d
jocularchao <jocularchao@gmail.com>
2023-11-29 17:46:10 +0800

    用户权限

--- study_mysql/02mysql basis/1.8DCL-01用户.sql
+++ study_mysql/02mysql basis/1.8DCL-01用户.sql
@@ -1,16 +1,32 @@
 
+-- 刷新权限
+flush privileges ;
 
 -- 1、创建用户
 -- CREATE USER 用户名 identified by 密码;
+   /* -  必须拥有mysql数据库的全局CREATE USER权限，或拥有INSERT权限。
+    - 只能创建用户，不能赋予权限。
+    - 用户名，注意引号：如 'user_name'@'192.168.1.1'
+    - 密码也需引号，纯数字密码也要加引号
+    - 要在纯文本中指定密码，需忽略PASSWORD关键词。要把密码指定为由PASSWORD()函数返回的混编值，需包含关键字PASSWORD*/
+
 create user jocualrchao identified by '123456';
 
 -- 可以不带密码
 -- CREATE USER 用户名;
 create user jocualrchao;
 
+-- 设置密码
+set password = password('123456');
+-- 为指定用户设置密码
+set password for jocualrchao = password('123456');
 
+-- 重命名用户
+-- rename user old_user to new_user
+rename user jouclarchao to jocularchao1;
 
-
+-- 删除用户
+drop user jocualrchao1;
 
 -- 2、用户登录
 -- 在管理员模式cmd中，进入使用的mysql目录bin中，登录
@@ -20,6 +36,14 @@ create user jocualrchao;
 
 -- 虽然此用户能够成功登录，但是并不能查看完整的数据库列表，这是因为此用户还没有权限！
 show databases;
+
+-- 查看指定用户权限 show grants for root@localhost;
+show grants for root;
+
+-- 查看当前用户权限
+show grants;
+show grants for current_user;
+show grants for current_user();
 
 
 -- 3、用户授权
